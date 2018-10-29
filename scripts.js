eea536e980df7b1cbd79e2ec3ac2b163b178597f
Matt Cobb <matt@reedsy.com>
2018-10-29 18:46:05 +0000

    Initial commit

---
+++ scripts.js
@@ -0,0 +1,34 @@
+$(document).ready(function() {
+
+  $('.placeholder').click(function() {
+    // Fade out
+    $('.placeholder').fadeOut();
+    $('.play-button').fadeOut();
+    $('.learn-more').fadeOut();
+    var iframe = document.createElement( "iframe" );
+    iframe.setAttribute( "frameborder", "0" );
+    iframe.setAttribute( "allowfullscreen", "" );
+    iframe.setAttribute( "src", "https://www.youtube.com/embed/lOfH89SYESE?rel=0&showinfo=0&autoplay=1" );
+
+    $(".youtube").append( iframe );
+  });
+
+  // Fixed nav on scroll
+  $(window).bind('scroll', function () {
+    if ($(window).scrollTop() > 84) {
+      $('header').addClass('nav-fixed');
+      $('.learn-more').fadeOut();
+    } else {
+      $('header').removeClass('nav-fixed');
+      $('.learn-more').fadeIn();
+    }
+  });
+
+  $('.smooth-scroll').on('click',function (e) {
+    e.preventDefault();
+    $('html, body').animate({
+       scrollTop: $($.attr(this, 'href')).offset().top - 84
+    }, 750);
+  });
+
+});
